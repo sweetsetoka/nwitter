d9cbaa06f2a329458ecf567919b4a943e9d379dc
sweetsetoka <sweetsetoka@gmail.com>
2021-09-28 22:53:55 +0900

    add router(chapter3)

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+//import logo from './logo.svg';
+//import './App.css';
+import AppRouter from "./Router";
+
+function App() {
+ 
+    return <AppRouter />
+
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
-import App from './App';
+import App from './components/App';
 //import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/compat/app";
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
